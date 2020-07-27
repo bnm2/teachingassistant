d0e3060c586a3b9b81c4c97ca38d875477dd15ed
Bruno Nunes de Mesquita <bnm2@cin.ufpe.br>
2020-07-27 14:56:03 -0300

    Atualizando o sistema para receber o login GitHub de cada aluno

--- ta-gui/src/app/app.component.ts
+++ ta-gui/src/app/app.component.ts
@@ -7,11 +7,12 @@ import { NgModule } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-   aluno: Aluno = {nome: "", cpf: "", email: ""};
+   aluno: Aluno = {nome: "Bruno", cpf: "", email: "", GitHub: ""};
 }
 
 export class Aluno {
   nome: string;
   cpf: string;
   email: string;
+  GitHub: string;
 }
